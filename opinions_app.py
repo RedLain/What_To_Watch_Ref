89d54aa235841cb6708d771d8d071e66d2e3bec2
Ruslan Shcherbakov <rodger.r.gold@gmail.com>
2026-08-31 16:18:44 +0300

    add html documents

--- opinions_app.py
+++ opinions_app.py
@@ -1,7 +1,9 @@
+# what_to_watch/opinions_app.py
+
 from datetime import datetime
 from random import randrange
 
-
+# Импортировать функцию render_template().
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
@@ -11,7 +13,6 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
 
 db = SQLAlchemy(app)
 
-
 class Opinion(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(128), nullable=False)
@@ -19,18 +20,21 @@ class Opinion(db.Model):
     source = db.Column(db.String(256))
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
 
-
 @app.route('/')
 def index_view():
     quantity = Opinion.query.count()
     if not quantity:
-        return 'В базе данных мнений о фильмах нет.'
+        return 'В базе данных записей нет.'
     offset_value = randrange(quantity)
-    
     opinion = Opinion.query.offset(offset_value).first()
-    # Вот он — возврат функции.
-    return render_template('index.html')
+    # Подключить шаблон opinion.html.
+    return render_template('opinion.html', opinion=opinion)
 
+
+@app.route('/add')
+def add_opinion_view():
+    # Подключить шаблон add_opinion.html.
+    return render_template('add_opinion.html')
 
 if __name__ == '__main__':
-    app.run()
+    app.run() 
