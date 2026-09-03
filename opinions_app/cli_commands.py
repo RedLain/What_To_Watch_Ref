cce4f4f3813a13b2460d4de65b536a40c14a3f03
Ruslan Shcherbakov <rodger.r.gold@gmail.com>
2026-09-03 12:25:49 +0300

    refactoring

---
+++ opinions_app/cli_commands.py
@@ -0,0 +1,19 @@
+import csv
+
+import click
+
+from opinions_app import app
+from opinions_app.models import Opinion, db
+
+@app.cli.command('load_opinions')
+def load_opinions_command():
+    """Функция загрузки мнений в базу данных."""
+    with open('opinions.csv', encoding='utf-8') as f:
+        reader = csv.DictReader(f)
+        counter = 0
+        for row in reader:
+            opinion = Opinion(**row)
+            db.session.add(opinion)
+            db.session.commit()
+            counter += 1
+    click.echo(f'Загружено мнений: {counter}')
---
+++ settings.py
@@ -0,0 +1,3 @@
+class Config(object):
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///db.sqlite3'
+    SECRET_KEY = 'RedLain'
