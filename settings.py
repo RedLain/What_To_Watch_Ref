cbf5520e753ab9d0827376472cfacac32a1ebe5f
Ruslan Shcherbakov <rodger.r.gold@gmail.com>
2026-09-03 13:28:35 +0300

    corrected

--- settings.py
+++ settings.py
@@ -1,3 +1,12 @@
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///db.sqlite3'
-    SECRET_KEY = 'RedLain'
+    SQLALCHEMY_DATABASE_URI = os.getenv(
+        'DATABASE_URI', default='sqlite:///db.sqlite3'
+    )
+    SECRET_KEY = os.getenv('SECRET_KEY', default='dummy_key')
